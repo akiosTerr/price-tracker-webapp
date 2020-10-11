5713a654a36708249cd7eada81f7652c0fccb60c
akios dev <akiosdev6@gmail.com>
2020-10-11 00:51:10 -0300

    added placeholder notification bell

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { MdNotifications } from 'react-icons/md';
 
 const Header = () => {
 	return (
 		<header className='App-header'>
 			<h1 className='Title'>Price Tracker</h1>
-			<button className='header-button'>
-				<p>ADD PRODUCT</p>
-			</button>
+			<div className='side-header'>
+				<div className='notificationbell-container'>
+					<p>3</p>
+					<MdNotifications id='notification-bell'></MdNotifications>
+				</div>
+				<button className='header-button'>
+					<p>ADD PRODUCT</p>
+				</button>
+			</div>
 		</header>
 	);
 };
--- src/components/item.tsx
+++ src/components/item.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { convertToFloat } from '../utils/utils';
 import api from '../services/ptrackr-api';
 import { VscLoading } from 'react-icons/vsc';
+import { FiExternalLink } from 'react-icons/fi';
 import './component-style/Item.css';
 import { ItemT } from '../App';
 
@@ -16,6 +17,7 @@ function Item(props: Item) {
 		current_pricetag,
 		previous_pricetag,
 		imageURL,
+		link,
 	} = props.item;
 	const [currentPt, setCurrentPT] = useState<string>();
 	const [previousPt, setPreviousPT] = useState<string>();
@@ -35,21 +37,33 @@ function Item(props: Item) {
 			setAnimState('spinning-anim');
 		}
 		setUpdateStatus(true);
-		api.get(`/products/${id}/getPrice`).then((res) => {
-			const data = res.data;
-			if (data.priceChange === true) {
-				console.log('new price received');
-				setCurrentPT(data.newPrice);
-				if (data.lastPrice) {
-					setPreviousPT(data.lastPrice);
+		api
+			.get(`/products/${id}/getPrice`)
+			.then((res) => {
+				const response = {
+					type: 'response',
+					data: res.data,
+				};
+				console.log(response);
+				const data = res.data;
+
+				if (data.priceChange === true) {
+					console.log('new price received');
+					setCurrentPT(data.newPrice);
+					if (data.lastPrice) {
+						setPreviousPT(data.lastPrice);
+					}
+				} else {
+					console.log('no price changes');
+					console.log(data);
 				}
-			} else {
-				console.log('no price changes');
-				console.log(data);
-			}
-			setAnimState('');
-			setUpdateStatus(false);
-		});
+				setAnimState('');
+				setUpdateStatus(false);
+			})
+			.catch((err) => {
+				console.log(err);
+				return false;
+			});
 	};
 
 	const calcPriceDifference = () => {
@@ -72,11 +86,17 @@ function Item(props: Item) {
 		calcPriceDifference();
 	}, [props]);
 
+	const goToLink = () => {
+		console.log('gotolink');
+	};
+
 	// todo: adjust product-image css to be a fixed size image
 	return (
 		<div className='card'>
 			<div className='card-header'>
-				<span></span>
+				<a target='_blank' href={link}>
+					<FiExternalLink id='externalLinkIcon' />
+				</a>
 				<p className='product-title'>{title}</p>
 				<button onClick={UpdateItem}>
 					<VscLoading id='loadingIcon' className={`${animState}`} />
